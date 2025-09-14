aa679367b0aa8a4f6b215e3dd9d6b9a775509040
Jegan Gnanasekaran <97190238+Jegansekaran@users.noreply.github.com>
2025-09-14 22:02:10 +0300

    Add files via upload

---
+++ TIMER/TIMER_0/TIMER_0.c
@@ -0,0 +1,68 @@
+#include <LPC214X.H>
+
+void pll(){
+
+	PLL0CON = 0X01;
+	
+	PLL0CFG = 0X01;
+	
+	PLL0FEED = 0XAA;
+	
+	PLL0FEED = 0X55;
+	
+	while(!(PLL0STAT & (1 << 10)));
+	
+	PLL0CON = 0X03;
+	
+	PLL0FEED = 0XAA;
+	
+	PLL0FEED = 0X55;
+	
+	VPBDIV = 0X01;
+}
+
+void tmr(){
+
+	T0CTCR = 0X00;
+	
+	T0TCR = 0X02;
+	
+	T0PR = 39999;
+	
+	T0TCR =0X01;
+}
+
+void delay(int i){
+
+	T0TCR = 0X02;
+	
+	T0TCR = 0X01;
+	
+	T0TC = 0;
+	
+	while(T0TC<i);
+	
+	T0TCR = 0X02;
+
+}
+
+void main(){
+	
+	pll();
+	tmr();
+	
+	IODIR0 = 0X01;
+	
+	while(1){
+	
+		IOSET0 = 0X1;
+		
+		delay(1001);
+		
+		IOCLR0 = 0X1;
+		
+		delay(1001);
+		
+	}
+
+}
---
+++ TIMER/TIMER_0/timer_0.d
@@ -0,0 +1,2 @@
+.\timer_0.o: TIMER_0.c
+.\timer_0.o: C:\my_keil\ARM\Inc\Philips\LPC214X.H
